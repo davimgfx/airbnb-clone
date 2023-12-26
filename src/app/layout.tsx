1767e6c47ffe650227ceb85fc05f2e5ecf033aa3
Davi Fernandes <davifncosta@gmail.com>
2023-12-26 07:54:04 -0300

    fix: layout to see the Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,31 @@
-import type { Metadata } from 'next'
-import { airbnbCereal } from '@/styles/fonts/airbnbCereal'
-import './globals.css'
+import type { Metadata } from "next";
+import { airbnbCereal } from "@/styles/fonts/airbnbCereal";
+import { Header } from "@/components";
+import { ToasterProvider } from "@/providers/ToastProvider";
+import { ClientOnly } from "./ClientOnly";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Airbnb | Vacation rentals, cabins, beach houses, unique homes & experiences',
-  description: 'Find the perfect place to stay at an amazing price in 191 countries. Belong anywhere with Airbnb.',
-}
+  title:
+    "Airbnb | Vacation rentals, cabins, beach houses, unique homes & experiences",
+  description:
+    "Find the perfect place to stay at an amazing price in 191 countries. Belong anywhere with Airbnb.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={airbnbCereal.className}>{children}</body>
+      <body className={airbnbCereal.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <Header />
+        </ClientOnly>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import logo from "@/../public/logo.svg";
-import Image from "next/image";
+
 export default function Home() {
   return (
     <>
-      <Image src={logo} alt="airbnb logo" width={130}/>
       <h2>Test</h2>
     </>
   );
